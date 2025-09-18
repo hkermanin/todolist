48f91e22d43a3d69c0117f53ab7bbf44321b6a71
kermani <97584991+hkermanin@users.noreply.github.com>
2025-09-18 11:51:54 +0000

    first commit

---
+++ todolist/src/App.jsx
@@ -0,0 +1,11 @@
+import Todo from "./components/todo";
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-200 flex justify-center items-center">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/components/todo.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import Task from "./task";
+
+export default function Todo() {
+  const [tasks, setTasks] = useState([
+    {id:uuidv4(),title:'test',state:true},
+    {id:uuidv4(),title:'test 2',state:false},
+    {id:uuidv4(),title:'test 3',state:true}
+  ]);
+
+  const addTask = (e)=>{
+    if(e.key === 'Enter' && e.target.value.trim() !== ''){
+      setTasks([...tasks,{id:uuidv4(),title:e.target.value,state:false}])
+      e.target.value = '';
+    }
+  }
+
+  function toggleTaskState(id){
+    const updatedTasks = tasks.map((task)=>{
+      if(task.id === id){
+        return {...task, state: !task.state}
+      }
+      return task;
+    })
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id){
+    const updatedTasks = tasks.filter((task)=> task.id !== id);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <div className="flex flex-col gap-5 w-[700px] p-5 shadow bg-white">
+      <h1 className="text-blue-600 font-bold text-3xl">
+        To-Do Project | Hossein Kermani
+      </h1>
+      <input
+        onKeyDown={addTask}
+        className="border border-gray-300 p-2 rounded outline-none"
+        placeholder="What needs to be done today?"
+        type="text"
+      />
+
+      <div className="max-h-[400px] overflow-y-auto">
+        {tasks.map((task, i)=>{
+          return <Task key={i} deleteTask={deleteTask} toggleTaskState={toggleTaskState} task={task}/>
+        })}
+        
+      </div>
+    </div>
+  );
+}
